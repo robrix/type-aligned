25ba396b7ba0ebe987432e4c483c8a3e11a44794
Rob Rix <rob.rix@me.com>
2018-05-12 19:44:47 -0400

    Define a constraint synonym to lift Show to categories.

--- Data/TASequence/Internal.hs
+++ Data/TASequence/Internal.hs
@@ -1,13 +1,16 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ConstraintKinds, ScopedTypeVariables #-}
 module Data.TASequence.Internal
 ( instShow
 , showsBinaryWith
 , showsUnaryWith
+, Forall2
 ) where
 
 import Data.Constraint
 import Data.Constraint.Forall
 import Data.Functor.Classes (showsBinaryWith, showsUnaryWith)
+
+type Forall2 c = ForallF (ForallF c)
 
 instShow :: forall c x y . ForallF (ForallF Show) c :- Show (c x y)
 instShow = Sub $ case (instF :: ForallF (ForallF Show) c :- ForallF Show (c x)) of
