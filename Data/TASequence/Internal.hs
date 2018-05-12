94c50ffa40a4caa02c6bc20402b4a5e47878ed86
Rob Rix <rob.rix@me.com>
2018-05-12 19:42:29 -0400

    Rename the graph parameter to c.

--- Data/TASequence/Internal.hs
+++ Data/TASequence/Internal.hs
@@ -9,7 +9,7 @@ import Data.Constraint
 import Data.Constraint.Forall
 import Data.Functor.Classes (showsBinaryWith, showsUnaryWith)
 
-instShow :: forall graph x y . ForallF (ForallF Show) graph :- Show (graph x y)
-instShow = Sub $ case (instF :: ForallF (ForallF Show) graph :- ForallF Show (graph x)) of
-  Sub Dict -> case (instF :: ForallF Show (graph x) :- Show (graph x y)) of
+instShow :: forall c x y . ForallF (ForallF Show) c :- Show (c x y)
+instShow = Sub $ case (instF :: ForallF (ForallF Show) c :- ForallF Show (c x)) of
+  Sub Dict -> case (instF :: ForallF Show (c x) :- Show (c x y)) of
     Sub Dict -> Dict
